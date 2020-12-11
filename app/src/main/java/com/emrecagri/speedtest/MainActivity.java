25466cc7a804aefd9260edf8ed947125398192f1
Emre Çağrı <58416774+emrecagri@users.noreply.github.com>
2020-12-11 21:35:33 +0300

    New commit

--- app/src/main/java/com/emrecagri/speedtest/MainActivity.java
+++ app/src/main/java/com/emrecagri/speedtest/MainActivity.java
@@ -1,13 +1,20 @@
 package com.emrecagri.speedtest;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.jignesh13.speedometer.SpeedoMeterView;
 
@@ -43,6 +50,64 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+
+    //menu kodları baslangici
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id =item.getItemId();
+        switch (id){
+
+            case R.id.hakkinda:
+
+                final AlertDialog.Builder hakkindapenceresi = new AlertDialog.Builder( MainActivity.this ); //hakkindapenceresi adında AlertDialog tanımladık
+                hakkindapenceresi.setTitle("Hakkında"); //AlertDialog penceresi başlığı
+                hakkindapenceresi.setMessage( "Bu uygulama \ngithub.com/emrecagri \ntarafından hazırlanmıştır." );         //AlertDialog penceresi açıklaması
+                hakkindapenceresi.setCancelable( true ); //true ile bu AlertDialog penceresinden seçeneklere basılmadan çıkış yapılabileceğini belirttik
+                hakkindapenceresi.setPositiveButton( "Geri", new DialogInterface.OnClickListener() { //AlertDialog Pozitif Geri seçeneği
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel(); // Geri seçeneği seçildiğinde bu kod çalışıp AlertDialog penceresini kapatacak
+                    }
+                });
+                AlertDialog hakkindapenceresiAlertDialogu = hakkindapenceresi.create();
+                hakkindapenceresiAlertDialogu.show(); // AlertDialog penceresini gösteren kod
+
+                break;
+
+
+            case R.id.cikis:
+
+                final AlertDialog.Builder cikispenceresi = new AlertDialog.Builder( MainActivity.this ); //cikispenceresi adında AlertDialog tanımladık
+                cikispenceresi.setMessage( "Uygulamadan çıkmak istediğinize emin misiniz ?" );         //AlertDialog penceresi açıklaması
+                cikispenceresi.setCancelable( true );  //true ile bu AlertDialog penceresinden seçeneklere basılmadan çıkış yapılabileceğini belirttik
+                cikispenceresi.setNegativeButton( "Hayır",new DialogInterface.OnClickListener() { //AlertDialog Negatif Hayır seçeneği
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel(); // Hayır seçeneği seçildiğinde bu kod çalışıp AlertDialog penceresini kapatacak
+                    }
+                });
+                cikispenceresi.setPositiveButton( "Evet", new DialogInterface.OnClickListener() { //AlertDialog Pozitif Evet seçeneği
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        finish();   // Evet seçeneği seçildiğinde bu kod çalışıp uygulamayı kapatacak
+                    }
+                });
+                AlertDialog cikispenceresiAlertDialogu = cikispenceresi.create();
+                cikispenceresiAlertDialogu.show(); // AlertDialog penceresini gösteren kod
+
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    //menu kodlari sonu
 
     class SpeedTestTask extends AsyncTask<Void,Void,Void>
     {
